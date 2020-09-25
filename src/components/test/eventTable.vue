462b40a67e0cc7c0acdef77c63715f5dded6d5a7
yl.hou <yl.hou@lyncustek.com>
2020-09-25 16:22:37 +0800

    componentize test page

---
+++ src/components/test/eventTable.vue
@@ -0,0 +1,193 @@
+<template>
+  <div class="table-cont">
+    <div class="table-actions">
+      <div>
+        <label for="perPage">每頁</label>
+        <select id="perPage" name="perPage">
+          <option value="10">10</option>
+          <option value="20">20</option>
+          <option value="30">30</option>
+          <option value="40">40</option>
+        </select>
+        <span>筆</span>
+      </div>
+
+      <div>
+        <el-input
+          class="search-button"
+          placeholder="輸入關鍵字"
+          v-model="searchText">
+          <i class="el-icon-search el-input__icon"
+            slot="suffix"
+            @click="handleSearch">
+          </i>
+        </el-input>
+      </div>
+
+      <div class="download-button">
+        <span class="button-text">下載所有資料</span>
+      </div>
+    </div>
+
+    <div class="table-rows-cont">
+      <div class="table-rows" 
+        v-for="(event, index) in events" 
+        :key="index">
+
+        <div class="table-row" >
+          <p>{{event.eventName}}</p>
+          <p>{{event.time.start}} ~ {{event.time.end}}</p>
+          <div class="event-actions">
+            <div class="event-button-blue" @click="handleEditPopup(event, index)">
+              <img src="../../assets/icons/edit.svg" />
+            </div>
+            <div class="event-button-blue">
+              <img src="../../assets/icons/location.svg" />
+            </div>
+            <div class="event-button-green">
+              <img src="../../assets/icons/download.svg" />
+            </div>
+            <div class="event-button-red">
+              <img src="../../assets/icons/delete.svg" />
+            </div>
+          </div>
+        </div>
+        <hr>
+      </div>
+
+      <div class="paging-cont">
+        <el-pagination
+          :page-size="10"
+          :pager-count="5"
+          layout="prev, pager, next"
+          :total="50"
+          :hide-on-single-page="true">
+        </el-pagination>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "eventTable",
+  props: ["events", "edittingPopup"],
+  components: {},
+  computed: {},
+  data() {
+    return {
+      searchText: ""
+    };
+  },
+  mounted() {},
+  methods: {
+    handleNewFormSubmit() {
+      this.$emit("handleNewFormSubmit", this.newEvent)
+    },
+    handleSearch() {
+
+    },
+    handleEditPopup() {
+      this.$emit("handleEditPopup", true)
+    },
+  }
+};
+</script>
+
+<style scoped>
+  .table-cont {
+    background-color: white;
+    padding: 40px 50px;
+    margin: 1.5vw 0;
+    border-radius: 10px;
+  }
+  .table-cont select {
+    margin: 0 0.5vw;
+  }
+  .table-actions {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+  .search-button >>> .el-input__inner {
+    border-radius: 35px;
+    border-color: #247262;
+    border-width: 2px;
+  }
+  .search-button >>> .el-input__suffix {
+    color: #247262;
+  }
+  .download-button {
+    border-radius: 35px;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    cursor: pointer;
+    background-color: #3FA893;
+    width: 130px;
+    height: 40px;
+    color: white;
+    font-weight: bold;
+  }
+  .table-rows-cont {
+    margin: 2em 0;
+  }
+  .table-row {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 8vw;
+  }
+  .table-row p {
+    margin-bottom: 0;
+  }
+  .event-actions {
+    display: flex;
+  }
+  .event-actions img {
+    height: 18px;
+  }
+  .event-button-blue , .event-button-green, .event-button-red {
+    margin: 0 5px;
+    padding: 4px 8px;
+    width: 30px;
+    height: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    cursor: pointer;
+  }
+  .event-button-blue {
+    background-color: #5d9cec;
+  }
+  .event-button-green {
+    background-color: #5fbeaa;
+  }
+  .event-button-red {
+    background-color: #f05050;
+  }
+  .table-rows hr {
+    border: 1px solid #4E8D80;
+    width: 80%;
+  }
+  .paging-cont >>> .el-pagination {
+    display: flex;
+    justify-content: center;
+  }
+  .paging-cont >>> .el-pagination .btn-next, .el-pagination .btn-prev {
+    color: #999A99;
+  }
+  .paging-cont >>> .el-pager li {
+    color: #ECECEC;
+  }
+  .paging-cont >>> .el-pager li.active {
+    background-color: #ECECEC;
+    color: #999A99;
+    border-radius: 5px;
+  }
+  .paging-cont >>> .el-pagination button:hover {
+    color: #3FA893;
+  }
+
+</style>
--- src/views/test.vue
+++ src/views/test.vue
@@ -13,150 +13,25 @@
         :dataList="functionMenu.data_list" 
       />
       <div class="function-content">
-
-        <!-- Add form -->
         <div class="shadow" style="border-radius: 10px;">
-          <div class="form-cont">
-            <form class="form">
-              <div class="form-row-cont">
-                <div class="form-rows">
-                  <label for="date">日期區間</label>
-                  <input type="date" id="date-start" name="date-start" v-model="newEvent.time.start">
-                  <span> ~ </span>
-                  <input type="date" id="date-end" name="date-end" v-model="newEvent.time.end">
-                </div>
-
-                <div class="form-rows">
-                  <label for="contractor">發包廠商</label>
-                  <input type="text" id="contractor" name="contractor" v-model="newEvent.eventData">
-                </div>
-
-                <div class="form-rows">
-                  <label for="location">施工地點</label>
-                </div>
-              </div>
-
-              <div class="form-rows">
-                <label for="content">內容說明</label>
-                <textarea id="content" name="content" v-model="newEvent.eventName"></textarea>
-              </div>
-            </form>
-            <div class="submit-button" @click="handleNewFormSubmit">
-              <span>+</span>
-              <span class="button-text">新增工程表單</span>
-            </div>
-          </div>
+          <AddEventForm 
+            :newEvent="newEvent"
+            @handleNewFormSubmit="handleNewFormSubmit"
+          />
         </div>
 
-        <!-- Bottom table -->
         <div class="shadow" style="border-radius: 10px;">
-          <div class="table-cont">
-            <div class="table-actions">
-              <div>
-                <label for="perPage">每頁</label>
-                <select id="perPage" name="perPage">
-                  <option value="10">10</option>
-                  <option value="20">20</option>
-                  <option value="30">30</option>
-                  <option value="40">40</option>
-                </select>
-                <span>筆</span>
-              </div>
-
-              <div>
-                <el-input
-                  class="search-button"
-                  placeholder="輸入關鍵字"
-                  v-model="searchText">
-                  <i class="el-icon-search el-input__icon"
-                    slot="suffix"
-                    @click="handleSearch">
-                  </i>
-                </el-input>
-              </div>
-
-              <div class="download-button">
-                <span class="button-text">下載所有資料</span>
-              </div>
-            </div>
-
-            <div class="table-rows-cont">
-              <div class="table-rows" 
-                v-for="(event, index) in events" 
-                :key="index">
-
-                <div class="table-row" >
-                  <p>{{event.eventName}}</p>
-                  <p>{{event.time.start}} ~ {{event.time.end}}</p>
-                  <div class="event-actions">
-                    <div class="event-button-blue" @click="handleEditPopup(event, index)">
-                      <img src="../assets/icons/edit.svg" />
-                    </div>
-                    <div class="event-button-blue">
-                      <img src="../assets/icons/location.svg" />
-                    </div>
-                    <div class="event-button-green">
-                      <img src="../assets/icons/download.svg" />
-                    </div>
-                    <div class="event-button-red">
-                      <img src="../assets/icons/delete.svg" />
-                    </div>
-                  </div>
-                </div>
-                <hr>
-              </div>
-            </div>
-
-            <div class="paging-cont">
-              <el-pagination
-                :page-size="10"
-                :pager-count="5"
-                layout="prev, pager, next"
-                :total="50"
-                :hide-on-single-page="true">
-              </el-pagination>
-            </div>
-          </div>
+          <EventTable
+            :events="events"
+            :edittingPopup="edittingPopup"
+            @handleEditPopup="handleEditPopup"
+          />
         </div>
 
-
-
-        <div class="edit-popup-cont" v-show="editPopup">
-          <div class="popup-background"></div>
-
-          <div class="form-cont-edit">
-            <img src="../assets/icons/close.svg" class="close-icon" 
-              @click="handleEditPopup"/>
-            <form class="form">
-              <div class="form-row-cont">
-                <div class="form-rows">
-                  <label for="date">日期區間</label>
-                  <input type="date" id="date-start-edit" name="date-start">
-                  <span> ~ </span>
-                  <input type="date" id="date-end-edit" name="date-end">
-                </div>
-
-                <div class="form-rows">
-                  <label for="contractor-edit">發包廠商</label>
-                  <input type="text" id="contractor-edit" name="contractor">
-                </div>
-
-                <div class="form-rows">
-                  <label for="location-edit">施工地點</label>
-                </div>
-              </div>
-
-              <div class="form-rows">
-                <label for="content-edit">內容說明</label>
-                <textarea id="content-edit" name="content"></textarea>
-              </div>
-            </form>
-            <div class="submit-button">
-              <span>+</span>
-              <span class="button-text">新增工程表單</span>
-            </div>
-          </div>
-        </div>
+        <EditFormPopup 
+          :edittingPopup="edittingPopup"
+          @handleEditPopup="handleEditPopup"
+        />
       </div>
     </div>
  
@@ -170,11 +45,18 @@
 <script>
 import BannerMenu from '@/components/disaster/banner.vue'
 import FunctionMenu from '@/components/disaster/functionMenu.vue'
+import AddEventForm from '@/components/test/addEventForm.vue'
+import EventTable from '@/components/test/eventTable.vue'
+import EditFormPopup from '@/components/test/editFormPopup.vue'
+
 export default {
   name: "test",
   components: {
     BannerMenu,
-    FunctionMenu
+    FunctionMenu,
+    AddEventForm,
+    EventTable,
+    EditFormPopup
   },
   data() {
     return {
@@ -185,7 +67,6 @@ export default {
         visible: true,
         data_list: []
       },
-      searchText: "",
       events: [
         {
           time: {
@@ -209,7 +90,7 @@ export default {
           eventName: '雲林辦事處測試'
         }
       ],
-      editPopup: false,
+      edittingPopup: false,
       newEvent: {
         time: {
           start: "",
@@ -232,15 +113,20 @@ export default {
     changeVisible(value){
       this.functionMenu.visible = value
     },
-    handleSearch(){
-      console.log(this.searchText)
+    handleEditPopup(value) {
+      this.edittingPopup = value
     },
-    handleEditPopup(e, i) {
-      console.log(e)
-      this.editPopup = !this.editPopup
-    },
-    handleNewFormSubmit() {
-      console.log(this.newEvent)
+    handleNewFormSubmit(newevent) {
+      console.log(newevent)
+      this.events.push(newevent)
+      this.newEvent = {
+        time: {
+          start: "",
+          end: ""
+        },
+        eventName: "",
+        eventData: ""
+      }
     }
   }
 };
@@ -264,175 +150,36 @@ export default {
   }
 
   /* Form */
-  .form-cont {
-    background-color: white;
-    padding: 2vw 3.5vw;
-    border-radius: 10px;
-  }
-  .form {
-    display: flex;
-  }
-  .form-row-cont {
-    flex: 1;
-  }
-  .form-rows {
-    margin-right: 1.5vw;
-    margin-bottom: 1.5vw;
-    display: flex;
-    flex: 1;
-  }
-  .form-rows label {
-    /* font-size: 1vw; */
-    padding-right: 1.7vw;
-  }
-  .form-rows input {
-    flex-grow: 1;
-  }
-  .form-rows input[type=text] {
-    padding: 5px 10px;
-  }
-  .form-rows textarea {
-    width: calc(100% - 7vw);
-    padding: 5px 10px;
-  }
-  .submit-button {
-    background-color: #3FA893;
-    border-radius: 35px;
-    width: 15vw;
-    height: 3.5vw;
-    display: flex;
-    align-items: center;
-    justify-content: space-evenly;
-    font-size: 1.5vw;
-    color: white;
-    font-weight: bold;
-    cursor: pointer;
-    margin: 0 auto;
-  }
+  
 
   /* Table */
-  .table-cont {
-    background-color: white;
-    padding: 2vw 3.5vw;
-    margin: 1.5vw 0;
-    border-radius: 10px;
-  }
-  .table-cont select {
-    margin: 0 0.5vw;
-  }
-  .table-actions {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-  }
-  .search-button >>> .el-input__inner {
-    border-radius: 35px;
-    border-color: #247262;
-    border-width: 2px;
-  }
-  .search-button >>> .el-input__suffix {
-    color: #247262;
-  }
-  .download-button {
-    border-radius: 35px;
-    display: flex;
-    align-items: center;
-    justify-content: space-evenly;
-    cursor: pointer;
-    background-color: #3FA893;
-    font-size: 1.3vw;
-    width: 13vw;
-    height: 40px;
-    color: white;
-    font-weight: bold;
-  }
-  .table-rows-cont {
-    margin: 2em 0;
-  }
-  .table-row {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    padding: 0 50px;
-  }
-  .table-row p {
-    margin-bottom: 0;
-  }
-  .event-actions {
-    display: flex;
-  }
-  .event-actions img {
-    height: 18px;
-  }
-  .event-button-blue , .event-button-green, .event-button-red {
-    margin: 0 5px;
-    padding: 4px 8px;
-    width: 30px;
-    height: 30px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 50%;
-    cursor: pointer;
-  }
-  .event-button-blue {
-    background-color: #5d9cec;
-  }
-  .event-button-green {
-    background-color: #5fbeaa;
-  }
-  .event-button-red {
-    background-color: #f05050;
-  }
-  .table-rows hr {
-    border: 1px solid #4E8D80;
-    width: 80%;
-  }
-  .paging-cont >>> .el-pagination {
-    display: flex;
-    justify-content: center;
-  }
-  .paging-cont >>> .el-pagination .btn-next, .el-pagination .btn-prev {
-    color: #999A99;
-  }
-  .paging-cont >>> .el-pager li {
-    color: #ECECEC;
-  }
-  .paging-cont >>> .el-pager li.active {
-    background-color: #ECECEC;
-    color: #999A99;
-    border-radius: 5px;
-  }
-  .paging-cont >>> .el-pagination button:hover {
-    color: #3FA893;
-  }
-
+  
   /* Edit Popup */
-  .popup-background {
-    background-color: #080808;
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    opacity: 0.7;
+  
+
+  /* RWD */
+  @media screen and (max-width: 1300px) {
+    .form {
+      display: block;
+    }
   }
-  .form-cont-edit {
-    background-color: white;
-    padding: 3.5vw 3.5vw 2vw 3.5vw;
-    border-radius: 10px;
-    position: absolute;
-    top: 0;
-    left: 0;
-    margin: 2vw;
-    width: calc(100% - 4vw);
+  @media screen and (max-width: 996px) {
+    .table-row {
+      padding: 0;
+    }
+    .table-rows hr {
+      width: 95%;
+    }
   }
-  .close-icon {
-    height: 20px;
-    width: 20px;
-    cursor: pointer;
-    position: absolute;
-    top: 20px;
-    right: 20px;
+  @media screen and (max-width: 768px) {
+    .table-row {
+      padding: 0 25px;
+      flex-direction: column;
+      align-items: start;
+    }
+    .event-actions[data-v-5752faac] {
+      align-self: center;
+      margin-top: 15px;
+    }
   }
 </style>
