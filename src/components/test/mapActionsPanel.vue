6afa44374d5d0feee8375fe0891d4ef23765e5d1
yl.hou <yl.hou@lyncustek.com>
2020-09-29 18:21:19 +0800

    testMap => mapActionsPanel UI update

--- src/components/test/mapActionsPanel.vue
+++ src/components/test/mapActionsPanel.vue
@@ -10,14 +10,30 @@
       class="actions-expanded-cont" 
       v-show="openedMapActions"
     >
-      <div class="action-cont" 
-        v-for="(mapAction, index) in mapActions" 
-        :key="index"
-        @mouseover="activateHover(index)"
-        @mouseleave="deactivateHover(index)"
-      >
-        <img v-bind:src="require(`@/assets/icons/map/${mapAction.img}.png`)" />        
-        <p>{{mapAction.text}}</p>
+      <div class="actions-main-cont">
+        <div class="action-cont" 
+          v-for="(mapAction, index) in mapActions" 
+          :key="index"
+          @mouseover="activateHover(index)"
+          @mouseleave="deactivateHover(index)"
+          @click="testMapActions(index)"
+        >
+          <img v-bind:src="require(`@/assets/icons/map/${mapAction.img}.png`)" />        
+          <p>{{mapAction.text}}</p>
+        </div>
+      </div>
+
+      <div class="actions-sub-cont" v-show="dragMode">
+        <div class="drag-mode-cont">
+          <div class="drag-action">
+            <img src="../../assets/icons/map/check-circle.png" />
+            <p>取消變更</p>
+          </div>
+          <div class="drag-action">
+            <img src="../../assets/icons/map/close-circle.png" />
+            <p>取消變更</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -32,6 +48,7 @@ export default {
   data() {
     return {
       openedMapActions: false,
+      dragMode: false,
       mapActions: [
         {
           img: "edit-location",
@@ -87,12 +104,18 @@ export default {
   methods: {
     toggleMapActions() {
       this.openedMapActions = !this.openedMapActions
+      this.dragMode = false
     },
     activateHover (i) {
       this.mapActions[i].img = this.mapActions[i].imgHover
     },
     deactivateHover (i) {
       this.mapActions[i].img = this.mapActions[i].imgUnhover
+    },
+    testMapActions(i) {
+      if (i===1) {
+        this.dragMode = !this.dragMode
+      }
     }
   }
 };
@@ -102,7 +125,7 @@ export default {
 
   .map-actions-cont {
     display: flex;
-    align-items: center;
+    align-items: flex-start;
     position: relative;
     padding: 15px;
   }
@@ -120,12 +143,23 @@ export default {
     width: 100%;
   }
   .actions-expanded-cont {
+    position: absolute;
+    left: 50px;
+    top: 20px;
+    /* display: flex; */
+    /* background-color: rgba(0, 0, 0, 0.3);
+    padding: 12px 10px 10px 45px;
+    border-radius: 15px;*/
+    /* position: absolute;
+    left: 45px;  */
+  }
+  .actions-main-cont {
     display: flex;
     background-color: rgba(0, 0, 0, 0.3);
-    padding: 12px 10px 10px 45px;
+    padding: 13px 10px 11px 45px;
     border-radius: 15px;
-    position: absolute;
-    left: 45px;
+    /* position: absolute;
+    left: 45px; */
   }
   .action-cont {
     display: flex;
@@ -145,19 +179,74 @@ export default {
     margin-bottom: 0;
     color: white;
   }
+
   .action-cont:hover p {
     color: #F2D36E;
   }
 
+  /* Drag Mode */
+  .actions-sub-cont {
+    background-color: rgba(0, 0, 0, 0.3);
+    padding: 12px 0;
+    border-radius: 15px;
+    width: 300px;
+    margin-left: 25px;
+    margin-top: 15px;
+  }
+  .drag-mode-cont {
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+  }
+  .drag-action {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 12px;
+    margin: 0 8px;
+    cursor: pointer;
+  }
+  .drag-action img {
+    height: 25px;
+  }
+  .drag-action p {
+    margin-bottom: 0;
+    color: white;
+    margin-left: 10px;
+  }
+
   @media screen and (max-width: 768px) {
-    .actions-expanded-cont {
+    .actions-main-cont {
       flex-direction: column;
       left: 20px;
       top: 35px;
       padding: 60px 3px 15px 3px;
     }
+    .actions-expanded-cont {
+      display: flex;
+      left: 20px;
+      top: 40px;
+    }
     .action-cont {
       margin: 6px 8px;
+    }
+    .actions-sub-cont {
+      width: auto;
+      height: 175px;
+      display: flex;
+      margin-left: 15px;
+      margin-top: 120px;
+    }
+    .drag-mode-cont {
+      flex-direction: column;
+    }
+    .drag-action {
+      flex-direction: column;
+      
+    }
+    .drag-action p {
+      margin-bottom: 10px;
+      margin-left: 0;
     }
   }
 </style>
